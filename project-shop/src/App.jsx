dff5cf02e7da7a0668599ff24b0a28dd649b3e0b
ZacW9102 <zacw9102@gmail.com>
2024-10-23 14:45:10 -0700

    styling update

--- project-shop/src/App.jsx
+++ project-shop/src/App.jsx
@@ -1,20 +1,43 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom"
 import Home from "./pages/Home"
 import Projects from "./pages/Projects"
+import Listings from "./pages/Listings"
+import Contact from "./pages/Contact"
 
 function App() {
   return (
     <BrowserRouter>
     <header>
-      <Link to="/">Home</Link>
+        <Link to="/">Home</Link>
       <nav>
-        <Link to="/projects">Projects</Link>
+        <NavLink 
+            to="/projects"
+            className={({isActive}) => isActive ? "active-link" : null}
+        >
+              Projects
+        </NavLink>
+        <NavLink 
+            to="/listings"
+            className={({isActive}) => isActive ? "active-link" : null}
+        >
+            Listings
+        </NavLink>
+        
+        <NavLink 
+            to="/contact"
+            className={({isActive}) => isActive ? "active-link" : null}
+        >
+            Contact
+        </NavLink>
       </nav>
     </header>
+    <div className="bar"></div>
     <Routes>
       <Route path='/' element={<Home />}/>
       <Route path='/projects' element={<Projects />}/>
+      <Route path='/listings' element={<Listings />}/>
+      <Route path='/contact' element={<Contact />}/>
     </Routes>
   </BrowserRouter>
   );
