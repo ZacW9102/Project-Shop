045bcb13fdce8c6f1acc72e43e197b2d74abfe99
ZacW9102 <zacw9102@gmail.com>
2024-10-24 12:55:14 -0700

    refactoring

--- project-shop/src/App.jsx
+++ project-shop/src/App.jsx
@@ -1,44 +1,22 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Projects from "./pages/Projects"
 import Listings from "./pages/Listings"
 import Contact from "./pages/Contact"
+import Layout from "./components/Layout"
 
 function App() {
   return (
     <BrowserRouter>
-    <header>
-        <Link to="/">Home</Link>
-      <nav>
-        <NavLink 
-            to="/projects"
-            className={({isActive}) => isActive ? "active-link" : null}
-        >
-              Projects
-        </NavLink>
-        <NavLink 
-            to="/listings"
-            className={({isActive}) => isActive ? "active-link" : null}
-        >
-            Listings
-        </NavLink>
-        
-        <NavLink 
-            to="/contact"
-            className={({isActive}) => isActive ? "active-link" : null}
-        >
-            Contact
-        </NavLink>
-      </nav>
-    </header>
-    <div className="bar"></div>
-    <Routes>
-      <Route path='/' element={<Home />}/>
-      <Route path='/projects' element={<Projects />}/>
-      <Route path='/listings' element={<Listings />}/>
-      <Route path='/contact' element={<Contact />}/>
-    </Routes>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='/projects' element={<Projects />}/>
+          <Route path='/listings' element={<Listings />}/>
+          <Route path='/contact' element={<Contact />}/>
+        </Route>
+      </Routes>
   </BrowserRouter>
   );
 }
