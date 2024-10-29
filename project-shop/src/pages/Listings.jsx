a008c2f3f9a86941eecb4749285edb51a73a580f
ZacW9102 <zacw9102@gmail.com>
2024-10-29 13:46:07 -0700

    adding and styling pages

--- project-shop/src/pages/Listings.jsx
+++ project-shop/src/pages/Listings.jsx
@@ -2,6 +2,8 @@ import React from "react"
 
 export default function Listings() {
     return (
-        <h1>Listings Page</h1>
+        <div className="listings-page">
+            <h1>Looks like there's no current listings avaliable!</h1>
+        </div>
     )   
 }
