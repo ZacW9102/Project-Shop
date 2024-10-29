36bbb541dbb027800ce40837e65aa0e3ce76fe3a
ZacW9102 <zacw9102@gmail.com>
2024-10-29 14:20:41 -0700

    restore from backup

--- project-shop/src/pages/Listings.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Listings() {
-    return (
-        <div className="listings-page">
-            <h1>Looks like there's no current listings avaliable!</h1>
-        </div>
-    )   
-}
